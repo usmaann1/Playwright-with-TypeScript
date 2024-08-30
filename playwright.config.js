5553db6a26f8b03526760093a5bc34c33f4021e7
NumairTariq <157149019+NumairTariq@users.noreply.github.com>
2024-08-30 23:04:29 +0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -46,7 +46,6 @@ const config = {
     permissions: ["clipboard-read"],
     video: "on",
     trace: "on",
-    viewport: null,
     launchOptions: {
       slowMo: 500, //
       args: ["--start-maximized"],
