b4cb847fd6f450ae4293cec48e822f755d37c74f
NumairTariq <157149019+NumairTariq@users.noreply.github.com>
2024-08-31 02:14:18 +0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,26 +1,56 @@
+// @ts-check
+const { devices } = require("@playwright/test");
 
-name: Playwright Tests
-on:
-  workflow_dispatch:
-jobs:
-  test:
-    timeout-minutes: 120
-    runs-on: ubuntu-latest
-   
-    steps:
-    - uses: actions/checkout@v4
-    - uses: actions/setup-node@v4
-      with:
-        node-version: lts/*
-    - name: Install dependencies
-      run: npm install
-    - name: Install Playwright Browsers
-      run: npx playwright install --with-deps
-    - name: Run Playwright tests
-      run: npx playwright test test-environment/test-cases/team-courses/team-courses.test.js --reporter=html
-    - uses: actions/upload-artifact@v4
-    - name: Upload Playwright report
-      uses: actions/upload-artifact@v4
-      with:
-        name: playwright-report
-        path: ./test-environment
+let outputDirectory = String("./test-environment/test-reports/");
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
+ */
+const config = {
+  testDir: "./test-environment/test-cases/",
+
+  /* Maximum time one test can run for. */
+  timeout: 600 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 15000,
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : undefined,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : 3,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  outputDir: `${outputDirectory}/trace-output/`,
+  reporter: [
+    ["html", { outputFolder: `${outputDirectory}/playwright-report/` }],
+  ],
+  /* Login session storage */
+  globalSetup: require.resolve("./test-environment/test-assets/global-setup"),
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    browserName: "chromium",
+    storageState: "loginState.json",
+    actionTimeout: 15000,
+    baseURL: "https://play.juicemind.com/",
+    ignoreHTTPSErrors: true,
+    headless: false,
+    permissions: ["clipboard-read"],
+    video: "on",
+    trace: "on",
+    viewport: null,
+    launchOptions: {
+      slowMo: 500, //
+      args: ["--start-maximized"],
+    },
+  },
+};
+
+module.exports = config;
