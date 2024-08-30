be0c6d10241ae097ebe392e2e07d22f62e2b27c9
NumairTariq <157149019+NumairTariq@users.noreply.github.com>
2024-08-31 00:30:15 +0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 // @ts-check
 const { devices } = require("@playwright/test");
 
-let outputDirectory = String("./test-environment/");
+let outputDirectory = String("./test-environment/test-reports/");
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -11,41 +11,41 @@ const config = {
   testDir: "./test-environment/test-cases/",
 
   /* Maximum time one test can run for. */
-  timeout: 8 * 60 * 1000,
+  timeout: 600 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 30000,
+    timeout: 15000,
   },
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 1,
+  retries: process.env.CI ? 2 : undefined,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   outputDir: `${outputDirectory}/trace-output/`,
   reporter: [
-    ["html", { outputFolder: `${outputDirectory}` }],
+    ["html", { outputFolder: `${outputDirectory}/playwright-report/` }],
   ],
   /* Login session storage */
   globalSetup: require.resolve("./test-environment/test-assets/global-setup"),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    viewport: { width: 2560, height: 1440 },
     browserName: "chromium",
     storageState: "loginState.json",
-    actionTimeout: 30000,
+    actionTimeout: 15000,
     baseURL: "https://play.juicemind.com/",
     ignoreHTTPSErrors: true,
-    headless: true,
+    headless: false,
     permissions: ["clipboard-read"],
     video: "on",
     trace: "on",
+    viewport: null,
     launchOptions: {
       slowMo: 500, //
       args: ["--start-maximized"],
