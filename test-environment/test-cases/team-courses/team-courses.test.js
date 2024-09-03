814f5e36967f5b8eeeec7c97a5fbc81e733d3b93
NumairTariq <157149019+NumairTariq@users.noreply.github.com>
2024-09-03 23:02:42 +0500

    Update team-courses.test.js

--- test-environment/test-cases/team-courses/team-courses.test.js
+++ test-environment/test-cases/team-courses/team-courses.test.js
@@ -12,6 +12,8 @@ test.describe("TestSuite: Team/Courses", () => {
   test("TC: Flow Test Python1", async ({ teamCoursesPage, browser }) => {
     const email = process.env.EMAIL_NUMAIR;
     const password = process.env.PASSWORD_NUMAIR;
+    console.log(email);
+    console.log(password);
     const randomName = await UserFunctions.generateName();
     const randomAssignment = await UserFunctions.generateName();
     const randomEmail = await UserFunctions.generateRandomEmail(email);
