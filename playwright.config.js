0e9db986c1401b4528a6738c2a79e2c385e21557
NumairTariq <157149019+NumairTariq@users.noreply.github.com>
2024-09-04 02:09:16 +0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   use: {
     viewport: {width:1920, height: 1080},
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: "https://play.juicemind.com/",
     headless:true,
     permissions: ['notifications'],
     screenshot:'on',
