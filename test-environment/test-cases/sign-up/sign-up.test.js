b61b15c3084f6c5c9162a23181c0a42d6c5829ab
Umair Khan <kmuhammadumair02@gmail.com>
2024-09-04 22:43:42 +0500

    FIxed Tests

--- test-environment/test-cases/sign-up/sign-up.test.js
+++ test-environment/test-cases/sign-up/sign-up.test.js
@@ -2,7 +2,7 @@ const { PlaywrightCore, UserFunctions } = require('../../../module-imports/helpe
 const { test, expect } = require('../../../module-imports/testFixtures.imports')
 import SignUpTestData from '../../test-assets/test-data-files/sign-up/sign-up-testData.json'
 import LoginTestData from '../../test-assets/test-data-files/login/login-testData.json'
-require('dotenv').config()
+import Credentials from "../../test-assets/test-data-files/Credentials/credentials.json";
 
 test.describe('TestSuite: Sign Up', () => {
 
