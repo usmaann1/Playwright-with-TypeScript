92221370840958378dc99a4d93f9274350349b66
Numair_Tariq <numairtariq1@gmail.com>
2024-09-05 21:21:09 +0500

    fixes

--- helper-functions/userFunctions.js
+++ helper-functions/userFunctions.js
@@ -253,4 +253,11 @@ exports.UserFunctions = class UserFunctions {
     console.log("Colors detected in the canvas:", hexColors);
     return hexColors;
   }
+
+  static async isShadeOfRed(hex) {
+    const red = parseInt(hex.substring(1, 3), 16);
+    const green = parseInt(hex.substring(3, 5), 16);
+    const blue = parseInt(hex.substring(5, 7), 16);
+    return red > green && red > blue;
+  }
 };
