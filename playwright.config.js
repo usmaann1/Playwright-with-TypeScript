fc81190573249d783c92566cff9e9790c3b62061
NumairTariq <157149019+NumairTariq@users.noreply.github.com>
2024-09-06 04:21:52 +0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 8 * 60 * 1000,
   use: {
-    viewport: null,
+    viewport: { width: 1920, height: 1080 },
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://play.juicemind.com/",
     headless:true,
