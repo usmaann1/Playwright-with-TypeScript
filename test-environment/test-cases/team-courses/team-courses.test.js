7f92ade93644fbfae8ae2101397546dd9113c202
NumairTariq <157149019+NumairTariq@users.noreply.github.com>
2024-09-06 05:40:30 +0500

    Update team-courses.test.js

--- test-environment/test-cases/team-courses/team-courses.test.js
+++ test-environment/test-cases/team-courses/team-courses.test.js
@@ -1,19 +1,389 @@
-const { PlaywrightCore, UserFunctions } = require('../../../module-imports/helperFunctions.imports')
-const { test, expect } = require('../../../module-imports/testFixtures.imports')
-import SignUpTestData from '../../test-assets/test-data-files/sign-up/sign-up-testData.json'
-import LoginTestData from '../../test-assets/test-data-files/login/login-testData.json'
+const { UserFunctions } = require("../../../module-imports/helperFunctions.imports");
+const { test } = require("../../../module-imports/testFixtures.imports");
+const { TeamCoursesPage } = require("../../../pages/team-courses/team-courses.page");
+import TeamCoursesData from "../../test-assets/test-data-files/team-courses/team-courses-testData.json";
 import Credentials from "../../test-assets/test-data-files/Credentials/credentials.json";
 
-test.describe('TestSuite: Sign Up', () => {
+test.describe("TestSuite: Team/Courses", () => {
+  const email = Credentials.EMAIL_NUMAIR;
+  const password = Credentials.PASSWORD_NUMAIR;
+  test.only("TC: Flow Test Python1", async ({ teamCoursesPage, browser }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    const randomEmail = await UserFunctions.generateRandomEmail(email);
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption8,
+      true
+    );
+    await teamCoursesPage.simplePython();
+    const link = await teamCoursesPage.PublishAndInvite();
+    const newPage = await browser.newPage();
+    const newTeamCoursesPageInstance = new TeamCoursesPage(newPage);
+    await newTeamCoursesPageInstance.afterInviteSignUp(
+      link,
+      randomEmail,
+      password,
+      TeamCoursesData.firstName,
+      TeamCoursesData.LastName
+    );
+    await newTeamCoursesPageInstance.createStarterCode(
+      TeamCoursesData.pythonTestInput,
+      true
+    );
+    await teamCoursesPage.assertingUserAnswered(TeamCoursesData.completeName);
+  });
 
-    test('Google homepage should have Google logo', async ({ page }) => {
-        // Navigate to Google homepage
-        await page.goto('https://www.google.com');
-      
-        // Verify the page title
-        await expect(page).toHaveTitle(/Google/);
-      
+  test("TC: Flow Test Java", async ({ teamCoursesPage, browser }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    const randomEmail = await UserFunctions.generateRandomEmail(email);
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption9,
+      true
+    );
+    await teamCoursesPage.simpleJava();
+    const link = await teamCoursesPage.PublishAndInvite();
+    const newPage = await browser.newPage();
+    const newTeamCoursesPageInstance = new TeamCoursesPage(newPage);
+    await newTeamCoursesPageInstance.afterInviteSignUp(
+      link,
+      randomEmail,
+      password,
+      TeamCoursesData.firstName,
+      TeamCoursesData.LastName
+    );
+    await newTeamCoursesPageInstance.createStarterCode(
+      TeamCoursesData.javaTestInput,
+      true
+    );
+    await teamCoursesPage.assertingUserAnswered(TeamCoursesData.completeName);
+  });
 
-      });
-    
-})
+  test("TC: Flow Test CSharp", async ({ teamCoursesPage, browser }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    const randomEmail = await UserFunctions.generateRandomEmail(email);
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption10,
+      true
+    );
+    await teamCoursesPage.simpleCsharp();
+    const link = await teamCoursesPage.PublishAndInvite();
+    const newPage = await browser.newPage();
+    const newTeamCoursesPageInstance = new TeamCoursesPage(newPage);
+    await newTeamCoursesPageInstance.afterInviteSignUp(
+      link,
+      randomEmail,
+      password,
+      TeamCoursesData.firstName,
+      TeamCoursesData.LastName
+    );
+    await newTeamCoursesPageInstance.createStarterCode(
+      TeamCoursesData.csharpTestInput,
+      true
+    );
+    await teamCoursesPage.assertingUserAnswered(TeamCoursesData.completeName);
+  });
+
+  test("TC: Flow Test C++", async ({ teamCoursesPage, browser }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    const randomEmail = await UserFunctions.generateRandomEmail(email);
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption11,
+      true
+    );
+    await teamCoursesPage.simpleCPP();
+    const link = await teamCoursesPage.PublishAndInvite();
+    const newPage = await browser.newPage();
+    const newTeamCoursesPageInstance = new TeamCoursesPage(newPage);
+    await newTeamCoursesPageInstance.afterInviteSignUp(
+      link,
+      randomEmail,
+      password,
+      TeamCoursesData.firstName,
+      TeamCoursesData.LastName
+    );
+    await newTeamCoursesPageInstance.createStarterCode(
+      TeamCoursesData.cppTestInput,
+      true
+    );
+    await teamCoursesPage.assertingUserAnswered(TeamCoursesData.completeName);
+  });
+
+  test("TC: Flow Test JavaScript", async ({ teamCoursesPage, browser }) => {
+    const randomEmail = await UserFunctions.generateRandomEmail(email);
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption1
+    );
+    await teamCoursesPage.createTest(
+      TeamCoursesData.createTestType,
+      TeamCoursesData.createTestOldType,
+      TeamCoursesData.createTestNewType,
+      TeamCoursesData.createTestName,
+      TeamCoursesData.testInput,
+      TeamCoursesData.testOutput
+    );
+    const link = await teamCoursesPage.PublishAndInvite();
+    const newPage = await browser.newPage();
+    const newTeamCoursesPageInstance = new TeamCoursesPage(newPage);
+    await newTeamCoursesPageInstance.afterInviteSignUp(
+      link,
+      randomEmail,
+      password,
+      TeamCoursesData.firstName,
+      TeamCoursesData.LastName
+    );
+    await newTeamCoursesPageInstance.createStarterCode(
+      TeamCoursesData.testInput
+    );
+    await teamCoursesPage.assertingUserAnswered(TeamCoursesData.completeName);
+    await teamCoursesPage.assertingUserAnswerHistory(
+      TeamCoursesData.completeName,
+      TeamCoursesData.testInput
+    );
+    await teamCoursesPage.uploadFile(TeamCoursesData.uploadFilePath);
+  });
+
+  test("TC: Flow Test HTML", async ({ teamCoursesPage, browser }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    const randomEmail = await UserFunctions.generateRandomEmail(email);
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.ProjectTypeOption12,
+      true
+    );
+    const link = await teamCoursesPage.simpleHTML();
+    const newPage = await browser.newPage();
+    const newTeamCoursesPageInstance = new TeamCoursesPage(newPage);
+    newTeamCoursesPageInstance.getHtmlData(
+      TeamCoursesData.HTMLTestOutput,
+      link
+    );
+  });
+
+  test("TC: Flow Test Python with Turtle", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption2
+    );
+    await teamCoursesPage.pythonWithTurtle();
+  });
+
+  test("TC: Flow Test Python with Tkinter", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption4
+    );
+    await teamCoursesPage.pythonWithTkinter();
+  });
+
+  test("TC: Flow Test Java with Swing", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.ProjectTypeOption13
+    );
+    await teamCoursesPage.javaWithSwing();
+  });
+
+  test("TC: Flow Test Python with Matplotlib", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption6
+    );
+    await teamCoursesPage.pythonWithMatplotlib();
+  });
+
+  test("TC: Upload Python file", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption8,
+      true
+    );
+    await teamCoursesPage.waitFortime(10000);
+    await teamCoursesPage.uploadFile(TeamCoursesData.PythonFile);
+    await teamCoursesPage.uploadProgrammingFile();
+  });
+
+  test("TC: Upload JAVA file", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption9,
+      true
+    );
+    await teamCoursesPage.waitFortime(10000);
+    await teamCoursesPage.uploadFile(TeamCoursesData.JavaFile);
+    await teamCoursesPage.uploadProgrammingFile();
+  });
+
+  test("TC: Upload CSharp file", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption10,
+      true
+    );
+    await teamCoursesPage.waitFortime(10000);
+    await teamCoursesPage.uploadFile(TeamCoursesData.CSharpFile);
+    await teamCoursesPage.uploadProgrammingFile();
+  });
+
+  test("TC: Upload CPP file", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption11,
+      true
+    );
+    await teamCoursesPage.waitFortime(10000);
+    await teamCoursesPage.uploadFile(TeamCoursesData.CPPFile);
+    await teamCoursesPage.uploadProgrammingFile();
+  });
+
+  test("TC: Upload JavaScript file", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption1
+    );
+    await teamCoursesPage.waitFortime(10000);
+    await teamCoursesPage.uploadFile(TeamCoursesData.JSFile);
+    await teamCoursesPage.uploadProgrammingFile();
+  });
+
+  test("TC: Upload HTML file", async ({ teamCoursesPage, browser }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.ProjectTypeOption12,
+      true
+    );
+    await teamCoursesPage.waitFortime(10000);
+    const link = await teamCoursesPage.uploadProgrammingFile(
+      TeamCoursesData.HTMLFile,
+      true
+    );
+    const newPage = await browser.newPage();
+    const newTeamCoursesPageInstance = new TeamCoursesPage(newPage);
+    newTeamCoursesPageInstance.getHtmlData(
+      TeamCoursesData.HTMLFileUploadTest,
+      link
+    );
+  });
+
+  test("TC: Upload CSV file", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption1
+    );
+    await teamCoursesPage.waitFortime(10000);
+    await teamCoursesPage.uploadFile(TeamCoursesData.CSVFile);
+    await teamCoursesPage.uploadCSV();
+  });
+
+  test("TC: Upload JPG file", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption1
+    );
+    await teamCoursesPage.uploadFile(TeamCoursesData.JPGFile);
+    await teamCoursesPage.AssertImages(TeamCoursesData.TestJPG);
+  });
+
+  test("TC: Upload PNG file", async ({ teamCoursesPage }) => {
+    const randomName = await UserFunctions.generateName();
+    const randomAssignment = await UserFunctions.generateName();
+    await teamCoursesPage.signInUser(email, password);
+    await teamCoursesPage.CreateTeam(randomName);
+    await teamCoursesPage.CreateAssignment(randomAssignment);
+    await teamCoursesPage.IntializeIDE(
+      randomName,
+      TeamCoursesData.projectTypeOption1
+    );
+    await teamCoursesPage.uploadFile(TeamCoursesData.PNGFile);
+    await teamCoursesPage.AssertImages(TeamCoursesData.TestPNG);
+  });
+});
