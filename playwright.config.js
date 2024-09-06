e98e4455d134c0f90e76a65a2c2f487937bfdb65
NumairTariq <157149019+NumairTariq@users.noreply.github.com>
2024-09-06 22:30:01 +0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -22,9 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 8,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['json', { outputFile: 'test-results/results.json' }]
-  ],
+  reporter: [['json', { outputFile: 'playwright-report.json' }], ['slack-ctrf']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 8 * 60 * 1000,
   use: {
