d0417868ac708ca7fd0c51c78882cc54fbce9eb4
Syed Hassaan Zafar <hz@Syeds-MacBook-Air.local>
2024-09-19 18:47:49 +0500

    includes title name addition

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries:0,
   /* Opt out of parallel tests on CI. */
-  workers: 8,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['json-summary', { outputFile: 'summary.json' }],
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 8 * 60 * 1000,
   use: {
-    viewport: { width: 1920, height: 1080 },
+    // viewport: { width: 1920, height: 1080 },
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://play.juicemind.com/",
     headless:true,
