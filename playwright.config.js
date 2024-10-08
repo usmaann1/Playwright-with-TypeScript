76ffdb6a4f5fc057109b23d45c47f814b23571bd
NumairTariq <numairtariq1@gmail.com>
2024-10-08 22:08:12 +0500

    config changes

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries:1,
+  retries:0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -30,8 +30,8 @@ export default defineConfig({
   use: {
     viewport: { width: 1920, height: 1080 },
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://play.juicemind.com/",
-    headless:false,
+    baseURL: "https://dev.juicemind.com/",
+    headless: true,
     actionTimeout: 15000,
     permissions: ['notifications'],
     screenshot: 'on',
