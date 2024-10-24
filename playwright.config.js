d9b86d13a1351b248883dcc39dfd0127edc5618d
NumairTariq <157149019+NumairTariq@users.noreply.github.com>
2024-10-24 23:49:28 +0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries:0,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: 13,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['json-summary', { outputFile: 'summary.json' }],
@@ -30,7 +30,7 @@ export default defineConfig({
   use: {
     viewport: { width: 1920, height: 1080 },
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://stg.juicemind.com/",
+    baseURL: "https://dev.juicemind.com/",
     headless: true,
     actionTimeout: 15000,
     permissions: ['notifications'],
